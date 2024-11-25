fc37b38ae3ad9f2ddbc8f3df724cf8df28d94672
AMehriniso <144653974+AMehriniso@users.noreply.github.com>
2024-11-25 23:10:11 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+# Остановить выполнение скрипта при ошибке
 set -e
+
+# Проверка, что переменная окружения DEPLOY_PATH задана
 if [ -z "$DEPLOY_PATH" ]; then
   echo "Error: DEPLOY_PATH is not set. Exiting..."
   exit 1
 fi
 
+# Проверка, что папка dist существует
+if [ ! -d "dist" ]; then
+  echo "Error: Directory 'dist' does not exist. Please build the project before deploying. Exiting..."
+  exit 1
+fi
+
 echo "Starting deployment..."
 
+# Создание директории, если она не существует
 echo "Ensuring deployment directory exists..."
 mkdir -p "$DEPLOY_PATH"
 
+# Копирование сгенерированных файлов в директорию деплоя
 echo "Copying files to $DEPLOY_PATH..."
 cp -r dist/* "$DEPLOY_PATH"
-
-# Проверка успешности копирования
-if [ $? -eq 0 ]; then
-  echo "Files successfully deployed to $DEPLOY_PATH."
-else
-  echo "Error during file deployment. Exiting..."
-  exit 1
-fi
 
 echo "Deployment completed successfully."
